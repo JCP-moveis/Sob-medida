c346827aa4148c75dec00d7ba2458d2b9d8af673
JCP-moveis <oficinamoveis@gmail.com>
2026-02-03 15:47:58 -0300

    Atualização para deixa site mais moderno

--- script.js
+++ script.js
@@ -229,5 +229,15 @@
 
     safeTrackClick(analyticsId);
   });
+  
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
 
 })();
